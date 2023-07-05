56b837d6cf4971676b89da42db6ec28f087b945f
AyoubMS <chahayb563@gmail.com>
2023-07-05 15:40:21 +0200

    add app

---
+++ WindowsFormsHomework/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsHomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.sayHiButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(80, 59);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(131, 29);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(80, 185);
+            this.lastNameLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(128, 29);
+            this.lastNameLabel.TabIndex = 1;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(221, 59);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(376, 35);
+            this.firstName.TabIndex = 2;
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(221, 182);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(376, 35);
+            this.lastName.TabIndex = 3;
+            // 
+            // sayHiButton
+            // 
+            this.sayHiButton.Location = new System.Drawing.Point(337, 273);
+            this.sayHiButton.Name = "sayHiButton";
+            this.sayHiButton.Size = new System.Drawing.Size(128, 44);
+            this.sayHiButton.TabIndex = 4;
+            this.sayHiButton.Text = "SayHi";
+            this.sayHiButton.UseVisualStyleBackColor = true;
+            this.sayHiButton.Click += new System.EventHandler(this.sayHiButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 749);
+            this.Controls.Add(this.sayHiButton);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.Button sayHiButton;
+    }
+}
+
